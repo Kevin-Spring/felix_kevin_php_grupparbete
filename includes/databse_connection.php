7c5d590e4f18828a095ee2b40f359b430fba2f55
Kevin Spring <kevinspring93@hotmail.com>
2020-02-19 13:13:31 +0100

    second commit

---
+++ includes/databse_connection.php
@@ -0,0 +1,23 @@
+<?php
+
+//Databasvariabler. PDO-PHP.
+//PHP VARIABEL-SETUP
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "blogg_grupparbete";
+
+
+//try och catch är för att kunna fånga errors och visa dem som meddlanden istället för att sidan kraschar totalt.
+//try är som det låter, man testar nått.
+//om det skiter fångar catch upp det med ett felmeddelande.
+
+//MAKE CONNECTION 
+try {
+    $dsn = "mysql:host=$host;dbname=$db;";
+    $dbh = new PDO($dsn, $user, $pass);
+} catch (PDOException $e) {
+    echo "Error!" .  $e->getMessage() . "<br />";
+    die;
+}
+?>
