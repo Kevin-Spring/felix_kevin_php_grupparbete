8124616a3ce3ea1dc81269557795d08e7e3d0708
Kevin Spring <kevinspring93@hotmail.com>
2020-02-24 14:40:50 +0100

    additional admin pages

--- views/adminPage.php
+++ views/adminPage.php
@@ -1,1 +1,16 @@
-<h1>Hej!</h1>
+<?php 
+    session_start();
+    if($_SESSION['role'] != "admin"){
+        echo "<h2>Access denied dude, nice try..</h2>";
+        die;
+    }
+?>
+
+<h1>Welcome Admin!</h1>
+
+<ul>
+    <li><a href="createPost.php">Create Post</a></li>
+    <li><a href="adminAbout.php">About</a></li>
+    <li><a href="adminContact.php">Contact</a></li>
+    <li><a href="logout.php">Sign out</a></li>
+</ul>
